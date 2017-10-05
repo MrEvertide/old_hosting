96c89eaa86938a36595567a25ef38f2b8a83b418
Cédric Nolin <ced.star.002@gmail.com>
2017-10-05 19:15:25 -0400

    Add basic controller and method for a server list

---
+++ app/Http/Controllers/ServerController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use \App\Server;
+
+class ServerController extends Controller
+{
+    public function serverList () {
+        return view('server/index');
+    }
+}
