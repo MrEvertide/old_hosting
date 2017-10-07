1e6479c124012edbe720449de5a3e35fd841b244
Cédric Nolin <ced.star.002@gmail.com>
2017-10-06 20:49:53 -0400

    Add a list of exisiting servers and a form to create a new one

---
+++ resources/views/server/add.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading"><h4>Add a server</h4></div>
+
+                <div class="panel-body">
+                    <form action="/servers/add" method="POST">
+			<input type="hidden" name="_token" value="{{ csrf_token() }}">
+			<div class="form-group">
+			    <label for="server_name">Name</label>
+			    <input type="text" name="server_name" id="server_name" class="form-control">
+			</div>
+			<div class="form-group">
+			    <label for="server_host">Host</label>
+			    <input type="text" name="server_host" id="server_host" class="form-control">
+			</div>
+			<div class="form-group">
+			    <label for="server_key">API token</label>
+			    <input type="text" name="server_key" id="server_key" class="form-control">
+			</div>
+			<button type="submit" class="form-control btn btn-primary">Add server</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
---
+++ resources/views/server/index.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+		    <h4 class="pull-left">Server List</h4>
+			<a class="btn btn-primary pull-right" href="/servers/add">Add a server</a>
+		    <div class="clearfix"></div>
+		</div>
+
+                <div class="panel-body">
+		    <table class="table">
+		    <tr>
+			<th>Name</th>
+			<th>Host</th>
+		    </tr>      
+                    @foreach ($servers as $server)
+		    <tr>
+			<td>{{$server['name']}}</td>
+			<td>{{$server['host']}}</td>
+		    </tr>
+		    @endforeach
+		    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
