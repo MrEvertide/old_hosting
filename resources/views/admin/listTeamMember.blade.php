b6f2fc36cbf80f406a0271027dacd67baede618c
Cédric Nolin <ced.star.002@gmail.com>
2017-10-19 22:32:29 -0400

    Move the user card to a partial template

--- resources/views/admin/listTeamMember.blade.php
+++ resources/views/admin/listTeamMember.blade.php
@@ -16,17 +16,7 @@
                     <div class="row">
                         @foreach($users as $user)
                             @if ($user->pivot->is_admin)
-                                <div class="col-sm-3 col-xs-12 team-member center-block text-center">
-                                    <div class="panel panel-default">
-                                        <div class="panel-heading">
-                                            <div class="team-member-name">{{$user->name}}</div>
-                                            <div class="team-member-email">{{$user->email}}</div>
-                                        </div>
-                                        <img style="height: 200px; border-radius: 250px; margin: 10px 0;" src="{{url('images/profile/'.$user->picture)}}">
-                                        <div class="panel-footer">
-                                        </div>
-                                    </div>
-                                </div>
+                                @include('partials/userCard')
                             @endif
                         @endforeach
                     </div>
@@ -35,18 +25,7 @@
                     <div class="row">
                         @foreach($users as $user)
                             @if (!$user->pivot->is_admin)
-                                <div class="col-sm-3 col-xs-12 team-member center-block text-center">
-                                    <div class="panel panel-default">
-                                        <div class="panel-heading">
-                                            <div class="team-member-name">{{$user->name}}</div>
-                                            <div class="team-member-email">{{$user->email}}</div>
-                                        </div>
-                                        <img style="height: 200px; border-radius: 250px; margin: 10px 0;" src="{{url('images/profile/'.$user->picture)}}">
-                                        <div class="panel-footer">
-                                            <a href="{{route("admin@viewTeamMember", [$user->id])}}" class="btn btn-primary">View user</a>
-                                        </div>
-                                    </div>
-                                </div>
+                                @include('partials/userCard')
                             @endif
                         @endforeach
                     </div>
---
+++ resources/views/partials/userCard.blade.php
@@ -0,0 +1,14 @@
+<div class="col-sm-3 col-xs-12 team-member center-block text-center">
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            <div class="team-member-name">{{$user->name}}</div>
+            <div class="team-member-email">{{$user->email}}</div>
+        </div>
+        <img style="height: 200px; border-radius: 250px; margin: 10px 0;" src="{{url('images/profile/'.$user->picture)}}">
+        <div class="panel-footer">
+            @if (!$user->pivot->is_admin)
+                <a href="{{route("admin@viewTeamMember", [$user->id])}}" class="btn btn-primary">View user</a>
+            @endif
+        </div>
+    </div>
+</div>
