b915611f1df236761d15cac7c85389bd598ee564
Cédric Nolin <ced.star.002@gmail.com>
2017-10-24 22:12:43 -0400

    Setup node correctly to import modules and fix font-awesome

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,7 +12,6 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
 </head>
 <body>
     <div id="app">
